211e2d1014ac1fad8f2d0677eb0be76a2fdd1e01
OM KUMAR PRIY <omkumarpriy@gmail.com>
2023-02-25 21:11:38 +0530

    Accio Constest 3

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -6,6 +6,13 @@ public class Order {
     private int deliveryTime;
 
     public Order(String id, String deliveryTime) {
+        this.id=id;
+        int n=Integer.parseInt(deliveryTime);
+        int m=n%100;
+        n=n/100;
+        n=n*60;
+        n=n+m;
+        this.deliveryTime=n;
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
@@ -17,3 +24,7 @@ public class Order {
 
     public int getDeliveryTime() {return deliveryTime;}
 }
+
+
+
+
